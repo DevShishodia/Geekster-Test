5a3f25afce2c389b2679206a2cd83fcb7da4abfd
DevShishodia <dshishodia56@gmai.com>
2021-12-05 06:39:09 -0800

    Change BG color

---
+++ Change background color/index.js
@@ -0,0 +1,11 @@
+let e = document.getElementById("back");
+let f = document.getElementsById("heading");
+
+f.innertext = "chbg"
+function change(color){
+    document.body.style.background = color;
+}
+function changecolor(){
+    change('coral');
+    
+}
